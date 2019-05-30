0c186f78eaba9a58467aecae642a5fa6d72529b4
adi <adityabhat@lazaro.in>
2019-05-30 14:57:09 +0530

    Initial Code

---
+++ js/form.js
@@ -0,0 +1,153 @@
+
+/*
+ * -----
+ * On submitting the Customer form, create a customer
+ * -----
+ */
+$( document ).on( "submit", ".js_registration_form", async function ( event ) {
+
+	/* -----
+	 * Prevent the default form submission behaviour
+	 * 	which triggers the loading of a new page
+	 ----- */
+	event.preventDefault();
+
+	var $form = $( event.target );
+	var domForm = $form.get( 0 );
+	var $formFieldsAndElements = $form.find( "input, textarea, select, button" );
+	var $feedbackMessage = $form.find( ".js_feedback_message" );
+
+	/* -----
+	 * Disable the form
+	 ----- */
+	$formFieldsAndElements.prop( "disabled", true );
+	$feedbackMessage.text( "" );
+
+	/* -----
+	 * Pull the data from the form
+	 ----- */
+		// Name
+	var $name = $form.find( "[ name = 'name' ]" );
+		// Phone number
+	var $phoneNumber = $form.find( "[ name = 'phoneNumber' ]" );
+		// Email address
+	var $emailAddress = $form.find( "[ name = 'emailAddress' ]" );
+
+	/* -----
+	 * Sanitize the data
+	 ----- */
+	// Name
+	$name.val( $name.val().trim() );
+	// Phone number
+	$phoneNumber.val( $phoneNumber.val().replace( /[^\d\s()-+]/g, "" ) );
+	// Email address
+	$emailAddress.val( $emailAddress.val().trim() );
+
+
+	/* -----
+	 * Validate the data
+	 ----- */
+	// Clear all error messages / indicators from the last submission
+	//  	( if there was one )
+	$form.find( ".form-error" ).removeClass( "form-error" );
+
+	// Name
+	if ( ! $name.val() )
+		$name.addClass( "form-error" );
+	// Phone number
+	if ( ! $phoneNumber.val().replace( /\D/g, "" ) )
+		$phoneNumber.addClass( "form-error" );
+	// Email address
+	if ( ! $emailAddress.val() || ( ! $emailAddress.val().includes( "@" ) ) )
+		$emailAddress.addClass( "form-error" );
+
+	// If the form has even one validation issue
+	// do not proceed
+	if ( $form.find( ".form-error" ).length ) {
+		$formFieldsAndElements.prop( "disabled", false );
+		$feedbackMessage.html( `
+			Please fill in the fields marked <span style="color: rgba( 255, 42, 46, 0.75 ); font-weight: 900">red</span>.
+		` );
+		return;
+	}
+	$feedbackMessage.text( "Registering you....." );
+
+	/* -----
+	 * Assemble the data
+	 ----- */
+	var information = { };
+	information.name = $name.val();
+	information.phoneNumber = $phoneNumber.val();
+	information.emailAddress = $emailAddress.val();
+
+	/* -----
+	 * Make the enquiry
+	 ----- */
+	 addRegistration( information )
+	 	.then( function () {
+	 		$feedbackMessage.text( "You're registration has been accepted! We’ll get back to you with the details." );
+	 	} )
+	 	.catch( function () {
+	 		$feedbackMessage.text( "Something went wrong. Please try again later." );
+	 	} )
+
+} );
+
+
+
+/*
+ *
+ * Handle error / exception response helper
+ *
+ */
+function getErrorResponse ( jqXHR, textStatus, e ) {
+	var statusCode = -1;
+	var message;
+	if ( jqXHR.responseJSON ) {
+		code = jqXHR.responseJSON.statusCode;
+		message = jqXHR.responseJSON.message;
+	}
+	else if ( typeof e == "object" ) {
+		message = e.stack;
+	}
+	else {
+		message = jqXHR.responseText;
+	}
+	return {
+		code,
+		message
+	};
+}
+
+
+
+/*
+ *
+ * Register a person
+ *
+ */
+function addRegistration ( information ) {
+
+	// Build the payload
+	var requestPayload = information;
+
+	var url = location.origin + "/server/registrations.php";
+
+	var ajaxRequest = $.ajax( {
+		url: url,
+		method: "POST",
+		data: requestPayload,
+		dataType: "json",
+	} );
+
+	return new Promise( function ( resolve, reject ) {
+		ajaxRequest.done( function ( response ) {
+			resolve( response );
+		} );
+		ajaxRequest.fail( function ( jqXHR, textStatus, e ) {
+			var errorResponse = getErrorResponse( jqXHR, textStatus, e );
+			reject( errorResponse );
+		} );
+	} );
+
+}
---
+++ pages/home.php
@@ -0,0 +1,223 @@
+<?php
+/*
+ *
+ * This is a sample page you can copy and use as boilerplate for any new page.
+ *
+ */
+
+// Page-specific preparatory code goes here.
+
+?>
+
+<?php require_once __DIR__ . '/../inc/above.php'; ?>
+
+
+
+
+
+<!-- Sample Section -->
+<section class="sample-section">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12">
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Sample Section -->
+
+
+
+
+<!-- Intro Section -->
+<section class="intro-section text-light fill-off-neutral space-one-top-bottom">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12 medium-6 medium-offset-1 large-4 large-offset-2 space-min">
+				<div class="logo space-half-bottom"><img class="block" src="media/logo-germinate-white.svg<?php echo $ver ?>"></div>
+				<div class="title h2 text-uppercase strong space-min-bottom">Net Worth Ladder</div>
+				<div class="h4 text-uppercase space-half-bottom">An audience with Saurabh Mukherjea</div>
+
+				<div class="p space-min-bottom">5 PM - 15th June - Conrad, Bangalore.</div>
+				<a class="button" href="#register">Register Now</a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Intro Section -->
+
+
+<!-- Event Section -->
+<section class="event-section space-one-top-bottom">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12 large-8 large-offset-2 xlarge-6 xlarge-offset-3">
+				<div class="h3 text-uppercase space-min-bottom text-center">The Low Risk Road to <strong>Stupendous Wealth</strong></div>
+			</div>
+			<div class="columns small-12 large-8 large-offset-2 space-min">
+				<div class="p text-center">Germinate in association with Marcellus gives you the chance to share a cup of coffee with the legendary portfolio manager and equity strategist. Be a part of an exclusive evening as <strong>Saurabh Mukherjea</strong> himself leads us on the low risk road to Stupendous Wealth.</div>
+			</div>
+			<div class="columns small-12 space-half-top-bottom space-min">
+				<div class="label text-uppercase strong text-center text-green space-min-bottom">Watch the event promo</div>
+				<div class="video-embed js_video_embed" data-src="niA4QLc-9mc">
+					<div class="video-loading-indicator"></div>
+				</div>
+			</div>
+			<div class="columns small-12 medium-6 large-5 large-offset-1 xlarge-4 xlarge-offset-2 space-half-bottom space-min">
+				<div class="row">
+					<div class="columns small-2">
+						<img class="block" src="media/icons/icon-book.svg<?php echo $ver ?>">
+					</div>
+					<div class="columns small-9 small-offset-1">
+						<div class="h4 text-uppercase space-min-bottom">Best Selling <br>Author</div>
+						<div class="p text-neutral">Bestselling author of Gurus of Chaos and The Unusual Billionaires, Saurabh Mukherjea’s latest bestseller Coffee Can Investing introduces us to a paradigm of investment that is potent and shockingly simple. </div>
+					</div>
+				</div>
+			</div>
+			<div class="columns small-12 medium-6 large-5 xlarge-4 space-half-bottom space-min">
+				<div class="row">
+					<div class="columns small-2">
+						<img class="block" src="media/icons/icon-money.svg<?php echo $ver ?>">
+					</div>
+					<div class="columns small-9 small-offset-1">
+						<div class="h4 text-uppercase space-min-bottom">10X the returns in 10 years.</div>
+						<div class="p text-neutral">What if you could make not 10 not 15 but 20 per cent compound annual growth rate (CAGR) on your investments? </div>
+					</div>
+				</div>
+			</div>
+			<div class="columns small-12 medium-6 large-5 large-offset-1 xlarge-4 xlarge-offset-2 space-half-bottom space-min">
+				<div class="row">
+					<div class="columns small-2">
+						<img class="block" src="media/icons/icon-chart-1.svg<?php echo $ver ?>">
+					</div>
+					<div class="columns small-9 small-offset-1">
+						<div class="h4 text-uppercase space-min-bottom">Lowered <br>risk</div>
+						<div class="p text-neutral">What if there was a way to grow your money four to five times whilst taking half the risk compared to the overall market?</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Event Section -->
+
+
+<!-- Publicity Section -->
+<section class="publicity-section fill-off-neutral space-one-top-bottom">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12 space-min">
+				<div class="h4 text-uppercase strong text-center">In the Media</div>
+			</div>
+		</div>
+		<!-- Video Carousel -->
+		<div class="row carousel space-half-bottom js_carousel_container">
+			<div class="video-list carousel-list text-light js_carousel_content">
+				<div class="video carousel-list-item js_carousel_item space-min">
+					<div class="video-embed js_video_embed" data-src="I23j04NfBco">
+						<div class="video-loading-indicator"></div>
+					</div>
+				</div>
+
+				<div class="video carousel-list-item js_carousel_item space-min">
+					<div class="video-embed js_video_embed" data-src="PljjhJRaJA4">
+						<div class="video-loading-indicator"></div>
+					</div>
+				</div>
+
+				<div class="video carousel-list-item js_carousel_item space-min">
+					<div class="video-embed js_video_embed" data-src="zG-9YsMrR1M">
+						<div class="video-loading-indicator"></div>
+					</div>
+				</div>
+
+				<div class="video carousel-list-item js_carousel_item space-min">
+					<div class="video-embed js_video_embed" data-src="FJwZmGVREEc">
+						<div class="video-loading-indicator"></div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-controls">
+				<div class="button prev fill-light js_pager" data-dir="left">Previous</div>
+				<div class="button next fill-light js_pager" data-dir="right">Next</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Publicity Section -->
+
+
+<!-- Register Section -->
+<section class="register-section space-one-top-bottom" id="register">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12 medium-8 medium-offset-2 large-4 large-offset-4 space-min">
+				<div class="h3 text-uppercase strong text-center space-min-bottom">Register Now</div>
+				<div class="p text-center space-min-bottom text-green">Book before the 7th of June and get an early bird discount on your tickets.</div>
+				<div class="h5 text-uppercase text-center space-half-bottom"><span class="text-neutral text-strike">₹1999</span> ₹499/-</div>
+			</div>
+
+			<form class="js_registration_form">
+				<div class="columns small-12 medium-8 medium-offset-2 large-4 space-min">
+					<label>
+						<span class="visuallyhidden">Full Name</span>
+					<input class="block" type="text" name="name" placeholder="Full Name">
+					</label>
+				</div>
+				<div class="columns small-12 medium-8 medium-offset-2 large-4 large-offset-0 space-min">
+					<label>
+						<span class="visuallyhidden">Mobile Number</span>
+					<input class="block" type="text" name="phoneNumber" placeholder="Mobile Number">
+					</label>
+				</div>
+				<div class="columns small-12 medium-8 medium-offset-2 large-4 space-min">
+					<label>
+						<span class="visuallyhidden">Email ID</span>
+					<input class="block" type="text" name="emailAddress" placeholder="Email ID">
+					</label>
+				</div>
+				<div class="columns small-12 medium-8 medium-offset-2 large-4 large-offset-0 space-min">
+					<label>
+						<span class="visuallyhidden">Register</span>
+					<button class="block fill-green" type="submit">Register</button>
+					</label>
+				</div>
+				<div class="columns small-12 medium-8 medium-offset-2 large-4 large-offset-4 space-min">
+					<div class="p text-center space-min-bottom js_feedback_message"></div>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+<!-- END: Register Section -->
+
+
+<!-- Contact Section -->
+<section class="contact-section fill-off-neutral space-one-top-bottom">
+	<div class="row">
+		<div class="container">
+			<div class="columns small-12 medium-10 medium-offset-1 large-8 large-offset-2 space-min">
+				<div class="p text-uppercase strong space-min-bottom">Call for Assistance</div>
+				<a class="h3 inline-middle strong text-green space-three-quarter-bottom" href="tel:+918197747676">+91 81977 47676</a>
+
+				<div class="space-three-quarter-bottom">
+					<div class="p space-min-bottom"><strong>Event Timings</strong>: <br>5PM Onwards, 15th of June.</div>
+					<div class="p"><strong>Location</strong>:<br>The Daffodil Hall, Conrad, Bangalore.</div>
+				</div>
+
+				<div class="row">
+					<a href="https://germinatewealth.com/" target="_blank" class="columns small-10 medium-3 space-min-top-bottom inline-middle"><img class="block" src="media/logo-germinate.svg<?php echo $ver ?>"></a>
+					<div class="columns small-10 medium-6 text-center space-min-top-bottom inline-middle">in association with</div>
+					<a href="http://marcellus.in/" target="_blank" class="columns small-10 medium-3 space-min-top-bottom inline-middle"><img class="block" src="media/logo-marcellus.svg<?php echo $ver ?>"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Contact Section -->
+
+
+
+
+
+<?php require_once __DIR__ . '/../inc/below.php'; ?>
